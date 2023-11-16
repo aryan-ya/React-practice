bf88638fe897063f5f7d81ff00fe21d97f665ee0
Aryan Kumar Yadav <ay3978943@gmail.com>
2023-11-16 09:49:27 +0530

    inde

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { OnlineBca } from './goonline/OnlineBca';
+
+function App() {
+  return (
+    <div className="App">
+    <OnlineBca />
+
+    </div>
+  );
+}
+
+
+export default App;
